33b4e2b75371f798764d5b42087e6e782544ad28
Geraldine <geraldine@rocketmal.com>
2026-02-26 11:27:26 +0000

    Add reduce.js

--- scripts/reduce.js
+++ scripts/reduce.js
@@ -7,7 +7,17 @@
 
 
 // Summing an array of numbers:
-
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr, 7);
+// To expand the function to see what is happening put a curly brace after the fat arrow and delete the single line function:
+//   console.log(
+//     "Accumulator:", acc,
+//     "Current Value:", curr,
+//     "Total:", acc + curr
+//   );
+//   return acc + curr;
+// }, 7);
+console.log(sum);
 
 const teamMembers = [
   {
@@ -33,6 +43,52 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
-
+let totalExperience = teamMembers.reduce((acc,curr) =>  acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+// { Developer: 12, Designer: 4 } <-- Desired ouput.
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {}); // The empty curly braces is the initial value in this case
+console.log(experienceByProfession);
+
+// Reduce challenge
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 73, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 93, english: 88, maths: 97, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 87, art: 95},
+    }
+];
+// The challenge is to return only the student with the higest english score in the format:
+// { name: 'Adam', max: 88 }
+const biggest = students.reduce((acc, curr) => {
+    return curr.results.english > acc.max ? ({ name: curr.name, max: curr.results.english}) : acc;
+}, { name: "", max: 0 });
+console.log(biggest);
